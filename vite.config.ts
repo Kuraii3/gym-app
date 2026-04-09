1b57c3c431e2476f2394653dc452cd20609e8bd3
Emmanuel John Umali <131749923+Kuraii3@users.noreply.github.com>
2026-04-09 20:45:07 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  base: '/gym-app/', // Added your repository name here
   resolve: {
     alias: {
       // Alias @ to the src directory
